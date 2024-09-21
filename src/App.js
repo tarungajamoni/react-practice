3d97c6ba79c6d2eb8f47f8d83045cc02d57e3273
tarungajamoni <saitarun.goud1998@gmail.com>
2024-09-21 16:44:54 +0530

    App refactor

--- src/App.js
+++ src/App.js
@@ -4,60 +4,42 @@ import RandomColor from "./components/random_color";
 import ToDo from "./components/todo/ToDo";
 import StarRating from "./components/star_rating";
 import ToDoList from "./components/todo_refactor/comp/ToDoList";
-
+const componentsMap = {
+  accordion: { component: Accordion, label: "Accordion" },
+  random: { component: RandomColor, label: "Random Color Generator" },
+  todo: { component: ToDo, label: "ToDo App" },
+  todoRefactored: { component: ToDoList, label: "ToDo_Refactored" },
+  star: { component: StarRating, label: "Star Rating" },
+};
 const App = () => {
   const [SApp, setApp] = useState("");
+
+  const SelectedComponent = SApp ? componentsMap[SApp].component : null;
+  console.log(SelectedComponent, "SelectedComponent");
   return (
     <>
       <div className="space-x-5">
-        <div className="m-5">Choose App</div>
         {SApp === "" ? (
           <>
-            <button
-              className="bg-blue-300 p-2 rounded-lg"
-              onClick={() => setApp("accordion")}>
-              Accordion
-            </button>
-            <button
-              className="bg-blue-300 p-2 rounded-lg"
-              onClick={() => setApp("random")}>
-              Random Color Generator
-            </button>
-            <button
-              className="bg-blue-300 p-2 rounded-lg"
-              onClick={() => setApp("todo")}>
-              ToDo
-            </button>
-            <button
-              className="bg-blue-300 p-2 rounded-lg"
-              onClick={() => setApp("todo_")}>
-              ToDo_Dup
-            </button>
-            <button
-              className="bg-blue-300 p-2 rounded-lg"
-              onClick={() => setApp("star")}>
-              Star Rating
-            </button>
+            <div className="m-5">Choose App</div>
+            {Object.keys(componentsMap).map((key) => (
+              <button
+                className="bg-blue-300 p-2 rounded-lg"
+                key={key}
+                onClick={() => setApp(key)}>
+                {componentsMap[key].label}
+              </button>
+            ))}
           </>
         ) : (
           <button
-            className="bg-blue-300 px-4 py-2 rounded-lg"
+            className="bg-blue-300 px-4 py-2 rounded-lg m-5"
             onClick={() => setApp("")}>
             Back
           </button>
         )}
       </div>
-      {SApp === "accordion" ? (
-        <Accordion />
-      ) : SApp === "random" ? (
-        <RandomColor />
-      ) : SApp === "todo" ? (
-        <ToDo />
-      ) : SApp === "todo_" ? (
-        <ToDoList />
-      ):SApp === "star" ? (
-        <StarRating />
-      ) : null}
+      {SelectedComponent && <SelectedComponent />}
     </>
   );
 };
