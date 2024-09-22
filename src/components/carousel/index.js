8d80fbbc5f4c51e2087666ca589e1c4ddbc00f5f
tarungajamoni <saitarun.goud1998@gmail.com>
2024-09-22 07:09:09 +0530

    carousel 2

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -7,38 +7,42 @@ const Carousel = () => {
     getImages();
   }, []);
   const getImages = async () => {
+    console.log("images");
     try {
       const response = await fetch(
         "https://picsum.photos/v2/list?page=2&limit=5"
       );
       const data = await response.json();
+      console.log(data, "data");
       setFetchdata(data);
     } catch (e) {
       console.log(e);
     }
   };
+  console.log(fetchData, "fetchData");
   return (
     <div className="flex justify-center flex-col">
-      {fetchData.map((item, index) => {
+      {/* {fetchData.map((item, index) => {
         return (
-          <>
-            <img
-              key={item.id}
-              src={item.download_url}
-              alt="img"
-              className={
-                currentSlide === index ? "h-[calc(100vh-10rem)]" : "hidden"
-              }
-            />
-          </>
+          <> */}
+      <img
+        key={fetchData[currentSlide]?.id}
+        src={fetchData[currentSlide]?.download_url}
+        alt="img"
+        // className={
+        //   currentSlide === index ? "h-[calc(100vh-10rem)]" : "hidden"
+        // }
+      />
+      {/* </>
         );
-      })}
+      })} */}
       <div className="flex justify-between mx-5">
         <button
           className="border bg-slate-500 p-2"
           onClick={() => {
             setCurrentSlide(
-              currentSlide === 0 ? fetchData.length - 1 : currentSlide - 1
+              // currentSlide === 0 ? fetchData.length - 1 : currentSlide - 1
+              currentSlide !== 0 && currentSlide - 1
             );
           }}>
           Prev
@@ -47,7 +51,8 @@ const Carousel = () => {
           className="border bg-slate-500 p-2"
           onClick={() => {
             setCurrentSlide(
-              currentSlide === fetchData.length - 1 ? 0 : currentSlide + 1
+              // currentSlide === fetchData.length - 1 ? 0 : currentSlide + 1
+              currentSlide !== fetchData.length - 1 && currentSlide + 1
             );
           }}>
           Next
